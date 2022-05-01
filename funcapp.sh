5f26cfe100774952866beeefeee569afaeb4ec2a
Le Hong Son <41584436+sonlh98ba@users.noreply.github.com>
2022-05-01 20:45:37 +0700

    Update funcapp.sh

--- funcapp.sh
+++ funcapp.sh
@@ -5,23 +5,6 @@ location='australiaeast'
 storageAccount="blob$uniqueId" 
 funcApp="funcapp$uniqueId"
 
-# # initiate local python environment
-# pipenv shell
-# pipenv install
-
-# # initiate a local project folder
-# func init function --python
-
-# cd function
-
-# # initiate a function
-# func new --name QueueTrigger --template "Azure Service Bus Queue trigger" --language python
-
-# func start
-
-# Create a resource group
-# az group create -n $resourceGroup -l $location
-
 # Create a storage account
 az storage account create \
     --name $storageAccount \
